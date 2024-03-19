7b748357df585dce239c0336c12d23be857e808f
towbes <towbes>
2024-03-18 22:35:35 -0500

    Change Feerrott aoc to lua

--- feerrott/Agent_of_Change.lua
+++ feerrott/Agent_of_Change.lua
@@ -1,21 +1,22 @@
 local fearplane_raid = {
     expedition = { name="Plane of Fear", min_players=1, max_players=40},
-    instance   = { zone="fearplane", version=0, duration=eq.seconds("8h") },
-    compass    = { zone="fearplane", x=-974.94, y=-1346.20, z=-500.81 },
-    safereturn = { zone="fearplane", x=-1019.46, y=-1366.74, z=-492.12, h=256 },
-    zonein     = { x=0, y=180, z=-4.12, h=256 }
+    instance   = { zone="fearplane", version=0, duration=eq.seconds("8h") }, -- zone lasts 8 hours
+    safereturn = { zone="fearplane", x=2491, y=-2159.26, z=4, h=227.0 }, --outside portal
+    zonein     = { x=1032, y=-834, z=100, h=260 },
 }
 
 function event_say(e)
-	local dz = e.self:GetExpedition()
+	local dz = e.other:GetExpedition()
 	if(e.message:findi("hail")) then
 		if dz.valid and dz:GetZoneName() == "fearplane" then
-			e.self:Say("Tell me when you're [" .. eq.saylink("ready") .. "] to enter")
+			e.self:Say("Tell me when you're [" .. eq.say_link("ready") .. "] to enter")
 		else
-			e.self:Say("Would you like to [" .. eq.saylink("request") ... "] the expedition?")
+			e.self:Say("Would you like to [" .. eq.say_link("request") .. "] the expedition?")
 		end
 	elseif(e.message:findi("request")) then
 		local dz = e.other:CreateExpedition(fearplane_raid)
+		dz:AddReplayLockout(eq.seconds("9h")) -- 9 hour lockout
+		e.self:Say("Tell me when you're [" .. eq.say_link("ready") .. "] to enter")
 	elseif(dz:GetZoneName() == "fearplane" and e.message:findi("ready")) then
 		e.other:MovePCDynamicZone("fearplane")
 	end
