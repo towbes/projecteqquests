9beb8d7de9c447be63adde3cb892487800a2aafa
towbes <towbes>
2024-03-20 07:59:38 -0500

    velious aocs in lua

---
+++ eastwastes/Agent_of_Change (Kael).lua
@@ -0,0 +1,23 @@
+local aoc_raid = {
+    expedition = { name="Kael Drakkel", min_players=1, max_players=72},
+    instance   = { zone="kael", version=0, duration=eq.seconds("8h") }, -- zone lasts 8 hours
+    safereturn = { zone="eastwastes", x=3571.81, y=-6273.32, z=152.33, h=227.0 }, --outside portal
+    zonein     = { x=-633, y=-47, z=125.56, h=260 },
+}
+
+function event_say(e)
+	local dz = e.other:GetExpedition()
+	if(e.message:findi("hail")) then
+		if dz.valid and dz:GetZoneName() == aoc_raid.instance.zone then
+			e.self:Say("Tell me when you're [" .. eq.say_link("ready") .. "] to enter")
+		else
+			e.self:Say("Would you like to [" .. eq.say_link("request") .. "] the expedition?")
+		end
+	elseif(e.message:findi("request")) then
+		local dz = e.other:CreateExpedition(aoc_raid)
+		dz:AddReplayLockout(eq.seconds("9h")) -- 9 hour lockout
+		e.self:Say("Tell me when you're [" .. eq.say_link("ready") .. "] to enter")
+	elseif(dz:GetZoneName() == aoc_raid.instance.zone and e.message:findi("ready")) then
+		e.other:MovePCDynamicZone(aoc_raid.instance.zone)
+	end
+end
---
+++ eastwastes/Agent_of_Change (Sleeper).lua
@@ -0,0 +1,23 @@
+local aoc_raid = {
+    expedition = { name="Sleepers Tomb", min_players=1, max_players=72},
+    instance   = { zone="sleeper", version=0, duration=eq.seconds("8h") }, -- zone lasts 8 hours
+    safereturn = { zone="eastwastes", x=130.77, y=-7448.90, z=163.28, h=227.0 }, --outside portal
+    zonein     = { x=0, y=0, z=5.62, h=260 },
+}
+
+function event_say(e)
+	local dz = e.other:GetExpedition()
+	if(e.message:findi("hail")) then
+		if dz.valid and dz:GetZoneName() == aoc_raid.instance.zone then
+			e.self:Say("Tell me when you're [" .. eq.say_link("ready") .. "] to enter")
+		else
+			e.self:Say("Would you like to [" .. eq.say_link("request") .. "] the expedition?")
+		end
+	elseif(e.message:findi("request")) then
+		local dz = e.other:CreateExpedition(aoc_raid)
+		dz:AddReplayLockout(eq.seconds("9h")) -- 9 hour lockout
+		e.self:Say("Tell me when you're [" .. eq.say_link("ready") .. "] to enter")
+	elseif(dz:GetZoneName() == aoc_raid.instance.zone and e.message:findi("ready")) then
+		e.other:MovePCDynamicZone(aoc_raid.instance.zone)
+	end
+end
