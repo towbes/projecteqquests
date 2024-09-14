0ee43c121bc95ff068565318b865bdb4f618f09e
AON Admin <aon@aon.com>
2024-09-14 13:22:12 -0500

    Remove Vulak Depop

--- templeveeshan/#Thylex_of_Veeshan.pl
+++ templeveeshan/#Thylex_of_Veeshan.pl
@@ -41,7 +41,7 @@ $check = 0;
     }
     elsif ($check == 0 && !defined $qglobals{vulak}) {
       quest::spawn2(124155,0,0,-739.4,517.2,121,510); # NPC: #Vulak`Aerr
-      quest::depop_withtimer();
+      #quest::depop_withtimer();
       quest::setglobal("vulak",1,2,"M$spawntime");
     }
   }
